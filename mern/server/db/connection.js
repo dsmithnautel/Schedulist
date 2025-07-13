42d499014bfac9860d2957ea356ce15f8b3041c6
orangeinkk <sam513h@gmail.com>
2025-07-13 12:24:53 -0400

    Update connection.js

--- mern/server/db/connection.js
+++ mern/server/db/connection.js
@@ -1,6 +1,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const URI = process.env.ATLAS_URI || "";
+const URI = process.env.MONGO_URI || "";
+
 const client = new MongoClient(URI, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -9,16 +10,21 @@ const client = new MongoClient(URI, {
   },
 });
 
-try {
-  // Connect the client to the server
-  await client.connect();
-  // Send a ping to confirm a successful connection
-  await client.db("admin").command({ ping: 1 });
-  console.log("Pinged your deployment. You successfully connected to MongoDB!");
-} catch (err) {
-  console.error(err);
+let db;
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    db = client.db("schedulist");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB:", err);
+  }
 }
 
-let db = client.db("employees");
+connectToDatabase();
 
 export default db;
+
